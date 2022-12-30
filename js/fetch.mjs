11127f6276f3daf1608374bb883d699dfeebeacb
andjhay <88853764+andjhay@users.noreply.github.com>
2022-12-30 15:22:13 +0100

    images

--- js/fetch.mjs
+++ js/fetch.mjs
@@ -1,4 +1,4 @@
-export async function fetchPosts(url) {
+export async function fetchPosts(url, options) {
   const response = await fetch(url);
   const results = await response.json();
   return results;
