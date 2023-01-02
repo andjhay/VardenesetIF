7cde587b4c0cef2c738116515bae621f08669bc4
andjhay <88853764+andjhay@users.noreply.github.com>
2023-01-02 20:30:37 +0100

    js and html layout

--- js/fetch.js
+++ js/fetch.js
@@ -2,7 +2,6 @@ export const postUrl = "https://vardenesetif.no/wordpress/wp-json/wp/v2/posts/";
 
 export async function getPosts() {
   const response = await fetch(postUrl);
-  console.log(response);
   const results = await response.json();
   return results;
 }
--- js/main.js
+++ js/main.js
@@ -4,9 +4,11 @@ console.log(posts);
 
 const pathOriginal = location.pathname;
 const path = pathOriginal.slice(pathOriginal.lastIndexOf("/") + 1);
-console.log(path);
+
 const newsContainer = document.querySelector("#newsContainer");
 
 posts.forEach((post) => {
-  newsContainer.innerHTML += `<h2>${post.title}</h2>`;
+  const dateFormat = `${post.date.slice(8, 10) + "-" + post.date.slice(5, 7) + "-" + post.date.slice(0, 4)}`;
+
+  newsContainer.innerHTML += `<div class="col container my-2"><div class="card p-3"><h2>${post.title.rendered}</h2><h5>${dateFormat}</h5>${post.content.rendered}</div></div>`;
 });
