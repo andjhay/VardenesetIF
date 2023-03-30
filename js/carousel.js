e36faa41806ff04b0d8de5298d5811b7fc889b6b
andjhay <88853764+andjhay@users.noreply.github.com>
2023-03-30 15:44:22 +0200

    image style

--- js/carousel.js
+++ js/carousel.js
@@ -8,19 +8,23 @@ export function loadCarousel(mediaData) {
   const carouselContainer = document.getElementById("carousel");
   carouselMedia.forEach((media, index) => {
     if (index == 0) {
-      carouselContainer.innerHTML += `<div class="carousel-item active shadow">
-<img
-  src="${media.source_url}"
-  alt="${media.alt_text}"
-/>
-</div>`;
+      carouselContainer.innerHTML += `
+      <div class="carousel-item active">
+        <img
+          class="rounded shadow"
+          src="${media.source_url}"
+          alt="${media.alt_text}"
+        />
+      </div>`;
     } else {
-      carouselContainer.innerHTML += `<div class="carousel-item shadow">
-    <img
-      src="${media.source_url}"
-      alt="${media.alt_text}"
-    />
-  </div>`;
+      carouselContainer.innerHTML += `
+      <div class="carousel-item">
+        <img
+          class="rounded shadow"
+          src="${media.source_url}"
+          alt="${media.alt_text}"
+        />
+      </div>`;
     }
   });
 }
